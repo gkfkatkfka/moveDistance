6cb5d66aec1ae64cb63fb1da16fffb7a6393efe9
gkfkatkfka <71181899+gkfkatkfka@users.noreply.github.com>
2021-11-30 00:48:55 +0900

    거리 구하는 기초

--- 크롤링/거리계산.py
+++ 크롤링/거리계산.py
@@ -10,6 +10,10 @@
 import pandas as pd
 import csv
 
+def switch(key):
+    local = {"잠실": 1, "고척": 2, "문학": 3, "수원": 4, "대전": 5, "광주": 6, "대구": 7, "사직": 8, "창원": 9}.get(key, 0)
+    return local
+
 # 필요한 리스트
 listYear=['2017', '2018', '2019', '2020', '2021'] # 연도 리스트
 
@@ -19,6 +23,9 @@ listTeam = ['NC','OB','KT','LG','WO','HT','LT','SS','SK','HH'] # 팀 리스트(n
 # 구단 별 이동거리 배열로 저장
 moveArr = []  # 이동거리 저장 배열
 
+# 거리 합
+distance = 0
+
 with open('../데이터/거리 엑셀.csv', 'r', encoding='UTF-8') as distanceFile:
     distanceRead = csv.reader(distanceFile)
     
@@ -27,3 +34,33 @@ with open('../데이터/거리 엑셀.csv', 'r', encoding='UTF-8') as distanceFi
         inArr = line.split(",")
         inArr[-1] = inArr[-1].replace("\n", "")
         moveArr.append(inArr)
+
+#print(moveArr)
+
+# 2017HH만 해보기
+f = open('../데이터/년도별/2017/2017HH.csv', 'r', encoding='UTF-8')
+
+# csv 파일 하나를 읽어옴
+information = csv.reader(f)
+
+# 헤더 데이터 안 읽어오기
+header = next(information)
+
+compareArr=[]
+
+# 정보 한 줄씩 불러오기
+for info in information:
+    compareArr.append(info[4])
+
+print(compareArr)
+
+for i in range(1,len(compareArr)-1):
+    #$print(compareArr[i])
+    row = switch(compareArr[i-1])
+    col= switch(compareArr[i])
+
+    distance += float(moveArr[row][col])
+    print(moveArr[row][col])
+
+print(distance)
+
