80309d5ad6894e6415ab3a62b3c3665cd7a3a64c
gkfkatkfka <71181899+gkfkatkfka@users.noreply.github.com>
2021-12-02 22:24:28 +0900

    분석 - 상관계수

---
+++ 분석/상관계수.py
@@ -0,0 +1,19 @@
+'''
+* 소스파일 이름 : 상관계수.py
+* 소스파일 목적 : 2017-2021년 상관계수 파악
+* 세부 목적
+: 2017-2021년 상관계수 구하기
+'''
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df =  pd.read_csv('../데이터/경기순위_이동거리/total.csv', encoding='UTF-8')
+
+corrData = df[['distance','rank']]
+
+# 두 변수간의 상관관계 분석
+corr1 =corrData['distance'].corr(corrData['rank'])
+corr2 = df['rank'].corr(df['distance'])
+
+#print(corrData)
+print(corr1)
