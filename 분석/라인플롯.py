96c5b6a31b20f1b38cdc5591d644307de6205774
gkfkatkfka <71181899+gkfkatkfka@users.noreply.github.com>
2021-12-02 17:45:40 +0900

    자료조사 끝

---
+++ 분석/라인플롯.py
@@ -0,0 +1,13 @@
+'''
+* 소스파일 이름 : 라인플롯.py
+* 소스파일 목적 : 라인플롯 그래프 그리기
+* 세부 목적
+: 년도별 라인 플롯 그래프 그리기
+: 'https://blog.naver.com/PostView.naver?blogId=youji4ever&logNo=222081942490&categoryNo=24&parentCategoryNo=&from=thumbnailList' 참고
+'''
+
+import seaborn as sns; sns.set()
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
--- 크롤링/경기승패.py
+++ 크롤링/경기승패.py
@@ -10,16 +10,31 @@ import re
 import csv
 import pandas as pd
 
-
 # 구장 식별 행, 열 반환
 def switch(key):
     local = {"잠실": 1, "고척": 2, "문학": 3, "수원": 4, "대전": 5, "광주": 6, "대구": 7, "사직": 8, "창원": 9}.get(key, 0)
     return local
 
-# 구장 별 이동거리 배열로 저장
-moveArr = []  # 이동거리 저장 배열
+# 팀 이름 변환
+def switchName(key):
+    local = { "OB": "두산", "키움": "WO", "넥센": "WO", "KIA": "HT", "삼성": "SS", "SSG": "SK", "한화": "HH"}.get(key, key)
+    #local = { "NC": "NC", "OB": "두산", "KT": "KT", "LG": "LG", "대전": 5, "광주": 6, "대구": 7, "사직": 8, "창원": 9}.get(key, key)
+    return local
 
-# 나중에 여기서 에러나면 거리 엑셀.csv가 경로에 잘 있는지 확인
+
+# 구장 별 이동거리에 사용
+moveArr = [] # 구장 별 이동거리 저장
+distance = 0 # 거리
+compareArr = []# 지역 담을 배열
+
+# 필요한 리스트
+listYear=['2017', '2018', '2019', '2020', '2021'] # 연도 리스트
+listTeam = ['KT','LG','WO','HT','LT','SS','HH','NC','SK','OB'] # 팀 리스트(nc, 두산, kt, lg, 키움, 기아, 롯데, 삼성, ssg, 한화)
+
+# 한글, 영어만 추출하는 정규식 => 팀명 추출할 때 사용
+string = re.compile('[^ 가-힣 a-z A-Z]+')
+
+# 거리 엑셀(크로스탭) 형식으로 되어 있는 것 불러오기
 with open('../데이터/이동거리/거리 엑셀.csv', 'r', encoding='UTF-8') as distanceFile:
 	distanceRead = csv.reader(distanceFile)
 	
@@ -29,101 +44,76 @@ with open('../데이터/이동거리/거리 엑셀.csv', 'r', encoding='UTF-8') 
 		inArr[-1] = inArr[-1].replace("\n", "")
 		moveArr.append(inArr)
 
-# 거리 합
-distance = 0
+for year in listYear:
+	for team in listTeam:
+		searchList = []
+		teamArr = [0, ]
+		compareArr = []
+		
+		# 파일 불러오기
+		f = open('../데이터/년도별/'+year+'/'+year+team+'.csv', 'r', encoding='UTF-8')
+		
+		next(f)
+		
+		# csv 파일 헤더 제외 읽어오기
+		information = csv.reader(f)
 
-# 지역 담을 배열
-compareArr = []
+	# 정보 한 줄씩 불러와서 비교 배열 넣기
+	for info in information:
+		# 거리 가져오기
+		compareArr.append(info[4])
+		
+		# 경기 내용(vs) 정보 가져오기
+		strData = info[3]
+		
+		# vs로 일차적으로 문자열 분할
+		strList = strData.split('vs')
 
-#
-teamArr=[0,]
-# 파일 불러오기
-f = open('../데이터/년도별/2017/2017HH.csv', 'r', encoding='UTF-8')
+		try:
+			# 숫자만 추출
+			firstScore = int(re.findall('\d+', strList[0])[0])
+			secondScore = int(re.findall('\d+', strList[1])[0])
+		except IndexError:
+			# 경기취소나 이런걸로 경기 안 했을 경우
+			firstScore = 0
+			secondScore = 0
+		
+		# 팀만 추출
+		firstTeam = string.sub('', strList[0])
+		secondTeam = string.sub('', strList[1])
+		
+		# 0:패배 , 1: 승리
+		if firstScore > secondScore and firstTeam == switchName(team):
+			temp = [info[1], info[2], info[3], 1]
+		elif firstScore < secondScore and secondTeam == switchName(team):
+			temp = [info[1], info[2], info[3], 1]
+		elif firstScore == secondScore:
+			temp = [info[1], info[2], info[3], -1]
+		else:
+			temp = [info[1], info[2], info[3], 0, ]
+		
+		searchList.append(temp)
 
-next(f)
-
-# csv 파일 헤더 제외 읽어오기
-information = csv.reader(f)
-
-# 정보 한 줄씩 불러와서 비교 배열 넣기
-for info in information:
-	compareArr.append(info[4])
-
-# 현재와 이전 구장 사이의 거리 구하기(moveArr 이용)
-for i in range(1, len(compareArr) - 1):
-	row = switch(compareArr[i - 1])
-	col = switch(compareArr[i])
+	# 현재와 이전 구장 사이의 거리 구하기(moveArr 이용)
+	for i in range(1, len(compareArr) - 1):
+		row = switch(compareArr[i - 1])
+		col = switch(compareArr[i])
+		
+		distance = float(moveArr[row][col])
 	
-	distance = float(moveArr[row][col])
-
-	teamArr.append(round(distance, 2))
+		teamArr.append(round(distance, 2))
 	
+		# csv 만들기
+		data = pd.DataFrame(searchList)
+		moveData = pd.DataFrame(teamArr)
+		
+		data = pd.concat([data, moveData], axis=1)
+		data.columns = ['year', 'date', '경기', '승패여부', '거리']
+		data.head()
+		data.to_csv('../데이터/년도별_승률_거리/'+year+team+'.csv', encoding='UTF-8', index=False)
 
 
-def switchName(key):
-    local = { "OB": "두산", "키움": "WO", "넥센": "WO", "KIA": "HT", "삼성": "SS", "SSG": "SK", "한화": "HH"}.get(key, key)
-    #local = { "NC": "NC", "OB": "두산", "KT": "KT", "LG": "LG", "대전": 5, "광주": 6, "대구": 7, "사직": 8, "창원": 9}.get(key, key )
-    return local
-
-def switch(key):
-    local = {"잠실": 1, "고척": 2, "문학": 3, "수원": 4, "대전": 5, "광주": 6, "대구": 7, "사직": 8, "창원": 9}.get(key, 0)
-    return local
 
 
-# 한글, 영어만 추출하는 정규식 => 팀명 추출할 때 사용
-string = re.compile('[^ 가-힣 a-z A-Z]+')
-
-# 파일 선택
-f = open('../데이터/년도별/2017/2017HH.csv', 'r', encoding='UTF-8')
-
-next(f)
-
-# csv 파일 하나를 읽어옴
-information = csv.reader(f)
-
-searchList=[]
-# 정보 한 줄씩 불러오기
-for info in information:
-	
-	strData = info[3]
-	
-	# vs로 일차적으로 문자열 분할
-	strList = strData.split('vs')
-	
-	print(strList)
-	
-	try:
-		# 숫자만 추출
-		firstScore = int(re.findall('\d+', strList[0])[0])
-		secondScore = int(re.findall('\d+', strList[1])[0])
-	except IndexError:
-		firstScore=0
-		secondScore=0
-
-	
-	# 팀만 추출
-	firstTeam = string.sub('', strList[0])
-	secondTeam = string.sub('', strList[1])
-	
-	# 0:패배 , 1: 승리
-	if firstScore > secondScore and firstTeam == "한화":
-		temp = [info[1],info[2], info[3], 1]
-	elif firstScore < secondScore and secondTeam == "한화":
-		temp = [info[1],info[2], info[3], 1]
-	elif firstScore==secondScore:
-		temp = [info[1], info[2], info[3], -1]
-	else:
-		temp = [info[1],info[2], info[3], 0,]
-	
-	searchList.append(temp)
-
-# csv 만들기
-data = pd.DataFrame(searchList)
-moveData=pd.DataFrame(teamArr)
-
-data=pd.concat([data,moveData],axis=1)
-data.columns = ['year','date', '경기', '승패여부','거리']
-data.head()
-data.to_csv('../데이터/년도별/2017/2017HHtest.csv', encoding='UTF-8',index=False)
 
 
